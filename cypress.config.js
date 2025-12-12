c7d71c1cfb4e545a896c6e240582d21b89816a58
blaise <blaise.ubanako@gmail.com>
2025-12-12 22:58:21 +0000

    update dependencies and stepdef path

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,31 @@
-const { defineConfig } = require("cypress");
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
-const {addCucumberPreprocessorPlugin} = require('@badeball/cypress-cucumber-preprocessor')
-const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild')
-
-
-async function setupNodeEvents(on, config) {
-  await addCucumberPreprocessorPlugin(on, config);
-  on('file:preprocessor',
-    createBundler({
-      plugins: [createEsbuildPlugin.default(config)],
-    })
-  );
-  return config;
-}
-
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: '**/*.{feature,features}',
-    setupNodeEvents
+    baseUrl: 'https://demoqa.com',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 30000,
+    video: true,
+    screenshotOnRunFailure: true,
+    specPattern: 'cypress/e2e/features/**/*.feature',
+    supportFile: 'cypress/support/e2e.js',
+    async setupNodeEvents(on, config) {
+      // Add Cucumber preprocessor plugin
+      await addCucumberPreprocessorPlugin(on, config);
+      // Add file preprocessor with esbuild
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    },
   },
 });
